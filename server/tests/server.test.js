db0038b9bb85aea212fe2a56f1cc30d5e5378db8
Gopal Nair <gnair@corp.costco.com>
2018-02-14 15:20:57 -0800

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+
+const todos = [
+    {text: 'First test todo'},
+    {text: 'Second test todo'}
+    ];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos)
+            .then(() => done(), (err) => done(err));
+        
+        });
 });
 
 describe('POST / todos', () => {
@@ -24,7 +34,7 @@ describe('POST / todos', () => {
                     return done(err);
                 }
 
-                Todo.find()
+                Todo.find({text})
                     .then((todos) => {
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
@@ -68,11 +78,10 @@ describe('POST / todos', () => {
                     
                 Todo.find()
                     .then((todos) => {
-                        expect(todos.length).toBe(0);
+                        expect(todos.length).toBe(2);
                         done();
                     })
-                    .catch((err) => {
-                        console.log('Inside Catch');
+                    .catch((err) => {                        
                         done(err);
                     });
 
@@ -82,3 +91,33 @@ describe('POST / todos', () => {
 
 
 });
+
+describe('GET /todos', () => {
+    it('should get all todos' , (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
+
+// describe('GET /todos', () => {
+//     it('should get all todos' , (done) => {
+//         request(app)
+//             .get('/todos')
+//             .expect(200)
+//             // .expect((res) => {
+//             //     expect(res.body.todos.length).toBe(2);
+//             // })
+//             .end((err,res) => {
+//                 if(err)
+//                     return done(err);
+//                 expect(res.body.todos.length).toBe(2);
+//                 done();
+                
+//             });
+//     });
+// });
